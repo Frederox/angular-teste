e433e58181100c5794ad82439c654a01ce7a7184
Frederox <fredericochessini@gmail.com>
2022-01-24 16:20:49 -0300

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,9 +7,8 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-  @Input() product! : Product;
+  @Input() product: Product | undefined;
   @Output() notify = new EventEmitter();
-  //@Input() product! : Product;
   constructor() { }
 
   ngOnInit() {
